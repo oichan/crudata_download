c51070f27b0a0232d01791cb05af432a62b5e8a5
[oiyusuke] <[yskksu11@gmail.com]>
2016-06-02 12:37:00 +0900

    You can determine the data you want from the command line like "python download.py temp"

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # crudata_download
-Download crudata
+This script can Download crudata(https://crudata.uea.ac.uk/cru/data/hrg/cru_ts_3.23/crucy.1506241137.v3.23/countries/)
+If you want other data, please change the url in "download.py".
--- download.py
+++ download.py
@@ -1,4 +1,6 @@
 #coding:utf-8
+import sys
+import os
 import re
 import urllib
 import urllib2
@@ -8,13 +10,19 @@ def match(html):
     url = [match.groups()[1] for match in matchs]
     return url
 
-if __name__ == "__main__":
-    url = "https://crudata.uea.ac.uk/cru/data/hrg/cru_ts_3.23/crucy.1506241137.v3.23/countries/pre/"
+def main(crudata):
+    url = "https://crudata.uea.ac.uk/cru/data/hrg/cru_ts_3.23/crucy.1506241137.v3.23/countries/"+crudata
     response = urllib.urlopen(url)
     html = response.read()
     csv_list = match(html)
     for filename in csv_list:
-        download_name="pre/"+filename
+        download_name=crudata+filename
         filename=url+filename
         urllib.urlretrieve(filename, download_name)
     print csv_list
+
+if __name__ == "__main__":
+    argvs = sys.argv
+    if os.path.isdir(argvs[1])==False:
+        os.makedirs(argvs[1])
+    main(argvs[1]+"/")
